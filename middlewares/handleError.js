d0b0fb01b83dce654e9729c1215f60ce6f9554ce
Anton Chernikov <000tony00053@gmail.com>
2023-06-15 19:11:14 +0300

    hotfix: исправить ошибки

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,10 +1,12 @@
 const userNotFound = { message: 'Запрашиваемый пользователь не найден' };
 const cardNotFound = { message: 'Карточка с указанным id не найдена.' };
 
-const handleError = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const handleError = (err, req, res, next) => {
   if (err.name === 'Error') {
     return res.status(404).send(req.path.includes('cards') ? cardNotFound : userNotFound);
-  } if (err.name === 'CastError' || 'ValidationError') {
+  }
+  if (err.name === 'CastError' || 'ValidationError') {
     return res.status(400).send({ message: 'Переданы некорректные данные' });
   }
   return res.status(500).send({ message: 'Что-то пошло не так' });
